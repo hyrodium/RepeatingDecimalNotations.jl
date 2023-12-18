efe539254441c57de3fbe159ef6e48b09038174f
hyrodium <hyrodium@gmail.com>
2023-12-18 22:49:30 +0900

    add src/RepeatingDecimalNotations.jl

---
+++ src/RepeatingDecimalNotations.jl
@@ -0,0 +1,43 @@
+module RepeatingDecimalNotations
+
+export @rd_str
+
+macro rd_str(str)
+    str = replace(str, r"(\d)_(\d)" => s"\1\2")
+    str = replace(str, r"(\d)_(\d)" => s"\1\2")
+    if !isnothing(match(r"^\d+$", str))
+        # "123"
+        return parse(Int, str)
+    elseif !isnothing(match(r"^\d*\.\d+$", str))
+        # "123.45"
+        dot_index = findfirst(==('.'), str)
+        integer_part = str[1:dot_index-1]
+        decimal_part = str[dot_index+1:end]
+        decimal_digits = length(decimal_part)
+        integer_num = parse(Int, integer_part)
+        decimal_num = parse(Int, decimal_part)
+        num = integer_num + decimal_num//(10^decimal_digits)
+        return num
+    elseif !isnothing(match(r"^\d*\.\d+\(\d+\)$", str))
+        # "123.45(678)"
+        dot_index = findfirst(==('.'), str)
+        left_index = findfirst(==('('), str)
+        integer_part = str[1:dot_index-1]
+        decimal_part = str[dot_index+1:left_index-1]
+        repeat_part = str[left_index+1:end-1]
+        (integer_part, decimal_part, repeat_part)
+        decimal_digits = length(decimal_part)
+        repeat_digits = length(repeat_part)
+        integer_num = parse(Int, integer_part)
+        decimal_num = parse(Int, decimal_part)
+        repeat_num = parse(Int, repeat_part)//(10^repeat_digits-1)
+        num = integer_num
+        num += decimal_num//(10^decimal_digits)
+        num += repeat_num//(10^decimal_digits)
+        return num
+    else
+        error("invalid input!")
+    end
+end
+
+end # module RepeatingDecimalNotations
