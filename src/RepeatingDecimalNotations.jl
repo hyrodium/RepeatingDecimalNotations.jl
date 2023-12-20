004a01b60cde639ac4de76ed1338b49d3acd8373
hyrodium <hyrodium@gmail.com>
2023-12-20 19:52:22 +0900

    fix bug in type depromotion

--- src/RepeatingDecimalNotations.jl
+++ src/RepeatingDecimalNotations.jl
@@ -80,7 +80,7 @@ end
 
 function _try_unpromote_type(r::Rational{BigInt})
     for T in (Int128, Int64, Int)
-        if typemin(T) < r.num < typemax(T) && typemin(T) < r.den < typemax(T)
+        if typemin(T) ≤ r.num ≤ typemax(T) && typemin(T) ≤ r.den ≤ typemax(T)
             r = Rational{T}(r)
         end
     end
