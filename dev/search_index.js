dd76b8f58935f222caa32d2384ec430985322779
Documenter.jl <documenter@juliadocs.github.io>
2023-12-20 13:55:05 +0000

    build based on f59b002

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#RepeatingDecimalNotations.jl","page":"Home","title":"RepeatingDecimalNotations.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"RepeatingDecimal","category":"page"},{"location":"#RepeatingDecimalNotations.RepeatingDecimal","page":"Home","title":"RepeatingDecimalNotations.RepeatingDecimal","text":"RepeatingDecimal\n\nIntermediate struct to represent a repeating decimal number.\n\n\n\n\n\n","category":"type"}]
+}
