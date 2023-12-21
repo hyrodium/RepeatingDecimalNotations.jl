1c4228f3f01b018b1c33c20ac736752c5c14caa1
hyrodium <hyrodium@gmail.com>
2023-12-21 23:11:33 +0900

    update `RepeatingDecimal` docstring

--- src/RepeatingDecimalNotations.jl
+++ src/RepeatingDecimalNotations.jl
@@ -12,6 +12,21 @@ abstract type RepeatingDecimalNotation end
     RepeatingDecimal
 
 Intermediate struct to represent a repeating decimal number.
+
+# Examples
+```jldoctest
+julia> RepeatingDecimal(false, 12743, 857142, 2, 6)
+       2|--|------|6
+    -127.43(857142)
+----------- --------------
+Finite part Repeating part
+
+julia> RepeatingDecimal(1//17)
+         0||----------------|16
+        +0.(0588235294117647)
+----------- -------------------
+Finite part Repeating part
+```
 """
 struct RepeatingDecimal
     sign::Bool  # true/false corresponds to +/-
