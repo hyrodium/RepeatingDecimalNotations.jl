28bf365a42a1a28ffb2928a1a8d2faefe004d1e3
hyrodium <hyrodium@gmail.com>
2023-12-22 23:22:44 +0900

    update

--- src/_ScientificNotation.jl
+++ src/_ScientificNotation.jl
@@ -88,33 +88,28 @@ function RepeatingDecimal(::ScientificNotation, _str::AbstractString)
         r_repeating = parse(BigInt, repeating_part)
         r_finite = parse(BigInt, integer_part*finite_part)
         return RepeatingDecimal(sign, r_finite, r_repeating, length(finite_part), length(repeating_part))
-    elseif !isnothing(match(r"^\d+\.r\d+$", str))
-        # "123.r45"
-        dot_index = findfirst(==('.'), str)
-        left_index = findfirst(==('r'), str)
-        integer_part = str[1:dot_index-1]
-        repeating_part = str[left_index+1:end]
-        r_integer = parse(BigInt, integer_part)
-        r_repeating = parse(BigInt, repeating_part)
-        r_finite = r_integer
-        return RepeatingDecimal(sign, r_finite, r_repeating, 0, length(repeating_part))
     elseif !isnothing(match(r"^\.\d+$", str))
         # ".45"
         finite_part = str[2:end]
         r_finite = parse(BigInt, finite_part)
         return RepeatingDecimal(sign, r_finite, big(0), length(finite_part), 1)
-    elseif !isnothing(match(r"^\.\d+r\d+$", str))
-        # ".45r678"
-        left_index = findfirst(==('r'), str)
-        finite_part = str[2:left_index-1]
-        repeating_part = str[left_index+1:end]
-        r_finite = parse(BigInt, finite_part)
-        r_repeating = parse(BigInt, repeating_part)
-        return RepeatingDecimal(sign, r_finite, r_repeating, length(finite_part), length(repeating_part))
+    end
+    m = match(r"^(\-|−?)\.(\d+)r(\d+)$", _str)
+    if !isnothing(m)
+        # .234r56e2
+        sign_str, decimal_str, repeat_str = m.captures
+        return _repeating_decimal_from_strings(sign_str, "", decimal_str, repeat_str)
+    end
+    m = match(r"^(\-|−?)(\d+)\.(\d*)r(\d+)$", _str)
+    if !isnothing(m)
+        # 1.234r56
+        # 1.r23
+        sign_str, integer_str, decimal_str, repeat_str = m.captures
+        return _repeating_decimal_from_strings(sign_str, integer_str, decimal_str, repeat_str)
     end
     m = match(r"^(\-|−?)\.r(\d+)$", _str)
     if !isnothing(m)
-        # .234r56
+        # .r45
         sign_str, repeat_str, = m.captures
         return _repeating_decimal_from_strings(sign_str, "", "", repeat_str)
     end
