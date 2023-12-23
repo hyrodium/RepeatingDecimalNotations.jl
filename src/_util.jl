d243b918ddb926f4d7b088e3bedf5015d803219f
hyrodium <hyrodium@gmail.com>
2023-12-23 14:45:49 +0900

    fix exponent term

--- src/_util.jl
+++ src/_util.jl
@@ -13,11 +13,6 @@ function _try_unpromote_type(r::Rational{BigInt})
     return r
 end
 
-function _repeating_decimal_from_strings(sign_str::AbstractString, integer_str::AbstractString, decimal_str::AbstractString, repeat_str::AbstractString, exponet_str::AbstractString)
-    rd = _repeating_decimal_from_strings(sign_str, integer_str, decimal_str, repeat_str)
-    return shift_decimal_point(rd, parse(Int, exponet_str))
-end
-
 function _repeating_decimal_from_strings(sign_str::AbstractString, integer_str::AbstractString, decimal_str::AbstractString, repeat_str::AbstractString)
     period = length(repeat_str)
     point_position = length(decimal_str)
@@ -25,4 +20,14 @@ function _repeating_decimal_from_strings(sign_str::AbstractString, integer_str::
     r_repeat = parse(BigInt, repeat_str)
     sign = sign_str in ("", "+")
     return RepeatingDecimal(sign, r_finite, r_repeat, point_position, period)
+end
+
+function _repeating_decimal_from_strings(sign_str::AbstractString, integer_str::AbstractString, decimal_str::AbstractString, repeat_str::AbstractString, exponet_str::AbstractString)
+    rd = _repeating_decimal_from_strings(sign_str, integer_str, decimal_str, repeat_str)
+    if exponet_str[1] == '−'
+        exponet_str = exponet_str[nextind(exponet_str, 1):end]
+        return shift_decimal_point(rd, -parse(Int, exponet_str))
+    else
+        return shift_decimal_point(rd, parse(Int, exponet_str))
+    end
 end
