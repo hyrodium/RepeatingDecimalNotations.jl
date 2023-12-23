3b53ac5030d12502887b0271df5590e996d6ebbf
hyrodium <hyrodium@gmail.com>
2023-12-23 14:54:50 +0900

    add `@rd_str` docstring

--- src/RepeatingDecimalNotations.jl
+++ src/RepeatingDecimalNotations.jl
@@ -16,6 +16,32 @@ include("_ParenthesesNotation.jl")
 include("_ScientificNotation.jl")
 include("_EllipsisNotation.jl")
 
+"""
+    @rd_str
+
+A string macro to create a rational number.
+
+# Examples
+```jldoctest
+julia> r = rd"123.4(56)"  # 123.4565656...
+61111//495
+
+julia> rd"1.234r56e2"  # Other notations
+61111//495
+
+julia> rd"123.45656..."  # are also supported.
+61111//495
+
+julia> float(r)  # Check floating point number approximation.
+123.45656565656566
+
+julia> rd"0.(9)"  # 0.999... is equal to 1.
+1//1
+
+julia> rd"0.99(9)", rd"1", rd"1.000_000"  # The notation of repeating decimals is not unique.
+(1//1, 1//1, 1//1)
+```
+"""
 macro rd_str(str)
     rd = RepeatingDecimal(str)
     r = rationalify(BigInt, rd)
